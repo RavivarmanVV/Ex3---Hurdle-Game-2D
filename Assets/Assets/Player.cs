1bcaef40b8961c75ddcba0b28728de9ca638400c
RavivarmanVV <ravirepublic16@gmai.com>
2025-11-11 19:54:01 +0530

    success

---
+++ Assets/Assets/Player.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /*public float speed,jumpforce;
+    private Rigidbody2D rd;
+    public Score cc;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float moveinp = Input.getAxisRaw("Horizontal");
+        transform.position += new Vector(moveinp ,0,0) * speed * *Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y)<0.001f)
+        {
+            rb.AddFprce(new Vector2(0,jumpforce),ForceMode2d,Impulse);
+        }
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.CompareTag("Destroy"))
+            {
+                cc.coincount++;
+                Destroy(other.gameObject);
+            }
+        }   
+    }*/
+}
